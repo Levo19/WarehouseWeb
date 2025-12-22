1b019841e380cdfd12916a8f786d1746dc126f5f
Levo19 <46749951+Levo19@users.noreply.github.com>
2025-12-22 00:48:41 -0500

    Update app.js

--- app.js
+++ app.js
@@ -466,7 +466,7 @@ class App {
                 result.data.forEach(p => {
                     // Optimize the image URL immediately upon storage
                     const stableImg = this.getOptimizedImageUrl(p.imagen);
-                    this.data.products[p.codigo] = { desc: p.descripcion, stock: p.stock, img: stableImg, min: p.min };
+                    this.data.products[p.codigo] = { codigo: p.codigo, desc: p.descripcion, stock: p.stock, img: stableImg, min: p.min };
                 });
 
                 // DATA DEBUG
